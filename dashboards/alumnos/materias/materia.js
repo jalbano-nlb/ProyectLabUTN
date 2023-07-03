3043f8883d4910a91d3045ea630624c83f6cac4c
jalbano-nlb <albanojoaco.jna@gmail.com>
2023-07-03 03:02:28 -0300

    Cards de clases con js

--- dashboards/alumnos/materias/materia.js
+++ dashboards/alumnos/materias/materia.js
@@ -23,6 +23,15 @@ var notasMateria = [
     {id:12, idMateria:3, calificacion: '6', instacia: 'Recuperatorio'},
 ]
 
+var asistencia = [
+    {id:1, asiste: 'P'},
+    {id:2, asiste: 'A'},
+    {id:3, asiste: 'P'},
+    {id:4, asiste: ''},
+    {id:5, asiste: ''},
+    {id:6, asiste: ''}
+]
+
 switch (materiaSelected) {
     case 1:
         document.getElementById('nombreMateria').innerHTML = 'Programacion III';
@@ -30,18 +39,23 @@ switch (materiaSelected) {
         generarInfoCatedras(1);
         generarHorariosCatedras(1);
         generarCalificaciones(1);
+        generarClases(1);
         break;
     case 2:
         document.getElementById('nombreMateria').innerHTML = 'Laboratorio III';
         document.title = 'Laboratorio III';
         generarInfoCatedras(2);
         generarHorariosCatedras(2);
+        generarCalificaciones(2);
+        generarClases(2);
         break;
     case 3:
         document.getElementById('nombreMateria').innerHTML = 'Inglés III';
         document.title = 'Inglés III';
         generarInfoCatedras(3);
         generarHorariosCatedras(3);
+        generarCalificaciones(3);
+        generarClases(3);
         break;
 
     default:
@@ -79,6 +93,37 @@ function generarCalificaciones(IDMateria) {
             contenedorNotas.appendChild(pNotas);
         }
     });
+
+}
+
+function generarClases(IDMateria){
+    
+    let contenedorClases = document.getElementById('listaClases');
+
+    asistencia.forEach(e => {
+        let _liCard = document.createElement('li');
+        _liCard.classList.add('card');
+
+        switch (e.asiste) {
+            case 'P':
+                _liCard.classList.add('card-presente');
+                break;
+            case 'A':
+                _liCard.classList.add('card-ausente');
+                break;
+            default:
+                _liCard.classList.add('card-pendiente');
+                break;
+        }
+        _liCard.innerHTML = `<h3>Clase: ${e.id}</h3>`;
+        contenedorClases.appendChild(_liCard);
+    })
+
+
+    // <li class="card">
+    //     <h3>Clase 1</h3>
+    // </li>
+
 
 }
 
